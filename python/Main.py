b3fdf665a183e8474d0d9d69a789abb8dedfe376
blackmune <dev@blackmune.com>
2020-06-26 21:57:26 -0600

    Can connect to account

--- python/Main.py
+++ python/Main.py
@@ -1,4 +1,11 @@
 import alpaca_trade_api as tradeapi
 from config import vars
 
-#api = tradeapi.REST()
+#set the api
+api = tradeapi.REST(vars["APCA_API_KEY_ID"], vars["APCA_API_SECRET_KEY"], api_version='v2')
+
+account = api.get_account()
+
+api.list_positions
+
+print(account)
