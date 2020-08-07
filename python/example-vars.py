be1c746f55353269025b75d31f9a5a62c856c191
blackmune@gmail.com <blackmune@gmail.com>
2020-08-06 22:18:02 -0600

    Expanded example.vars and added vscode python launch

--- python/example-vars.py
+++ python/example-vars.py
@@ -1,5 +1,9 @@
 vars = {
-    "APCA_API_BASE_URL": "https://api.alpaca.markets",
-    "APCA_API_KEY_ID": "<KEY_ID>",
-    "APCA_API_SECRET_KEY": "<SECRET_ID>",
+        "APCA_API_BASE_URL": "https://api.alpaca.markets",
+    "APCA_API_KEY_ID": "<key_id>",
+    "APCA_API_SECRET_KEY": "<secret_key>",
+    "APCA_API_DATA_URL": "https://data.alpaca.markets",
+    "APCA_RETRY_MAX": "3",
+    "APCA_RETRY_WAIT": "3",
+    "APCA_RETRY_CODES": "429,504",
 }
